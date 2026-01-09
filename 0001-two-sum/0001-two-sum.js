474ea0ce123ab6a70fecd50e9560d7e026b4f85c
Haran Ck <harankrishna03@gmail.com>
2026-01-09 12:59:58 +0530

    Time: 0 ms (100%), Space: 55.7 MB (51.14%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let map = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        let value = target - nums[i]
+        if(map.has(value)){
+            return [map.get(value),i]
+        }
+        map.set(nums[i],i)
+    }
+};
