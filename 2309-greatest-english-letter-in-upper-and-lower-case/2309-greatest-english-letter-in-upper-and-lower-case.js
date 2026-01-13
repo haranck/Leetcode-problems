0ebcad3a043b0c1756a496acfa3e8b5fbc36e8aa
Haran Ck <harankrishna03@gmail.com>
2026-01-13 23:02:29 +0530

    Time: 26 ms (7.81%), Space: 63.3 MB (7.81%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.js
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.js
@@ -3,19 +3,8 @@
  * @return {string}
  */
 var greatestLetter = function(s) {
-    const capital = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    const small = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-
-    const set = new Set()
+    const set = new Set(s)
     const arr = []
-    for(let char of s){
-        if(set.has(char)){
-            continue
-        }else{
-            set.add(char)
-        }
-    }
-    console.log(set)
     for(let char of s){
         if(set.has(char)){
             let cap = char.toUpperCase()
@@ -25,13 +14,7 @@ var greatestLetter = function(s) {
             }
         }
     }
-    console.log(arr)
+    if(arr.length === 0)return ''
     arr.sort((a,b)=>a.localeCompare(b))
-    console.log(arr)
-    if(arr.length === 0){
-        return ''
-    }else {
-
-        return arr[arr.length-1]
-    }
+    return arr[arr.length-1]
 };
