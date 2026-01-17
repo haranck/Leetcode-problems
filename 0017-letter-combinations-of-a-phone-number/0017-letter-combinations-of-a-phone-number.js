818878c06c66c7c6638a714cbc4c939ffa532982
Haran Ck <harankrishna03@gmail.com>
2026-01-17 09:18:41 +0530

    Time: 0 ms (100%), Space: 53.5 MB (79.85%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if (!digits.length) return [];
+
+    const map = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    };
+
+    let result = ['']; 
+
+    for (let digit of digits) {
+        let temp = [];
+        for (let combo of result) {
+            for (let ch of map[digit]) {
+                temp.push(combo + ch);
+            }
+        }
+        result = temp;
+    }
+
+    return result;
+};
