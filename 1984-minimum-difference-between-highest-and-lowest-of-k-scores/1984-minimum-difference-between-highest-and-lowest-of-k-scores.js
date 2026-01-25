b0d19ed5a7f451c061f81efed89ddb74a7caef71
Haran Ck <harankrishna03@gmail.com>
2026-01-25 14:43:08 +0530

    Time: 10 ms (43.27%), Space: 57.8 MB (42.1%) - LeetHub

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minimumDifference = function(nums, k) {
+    if (k === 1) return 0;
+
+    nums.sort((a, b) => a - b);
+    let min = Infinity
+
+    for (let i = 0; i <= nums.length - k; i++) {
+        let diff = nums[i + k - 1] - nums[i];
+        min = Math.min(min, diff);
+    }
+
+    return min;
+};
+
