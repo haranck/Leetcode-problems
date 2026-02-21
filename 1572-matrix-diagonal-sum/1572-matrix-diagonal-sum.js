ae6c8506d4f01b1151ed06900a04e4db2e9cc405
Haran Ck <harankrishna03@gmail.com>
2026-02-21 19:24:20 +0530

    Time: 2 ms (5.58%), Space: 56.5 MB (12.09%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function (mat) {
+    let len = mat.length
+    let sum = 0
+    for (let i = 0; i < len; i++) {
+        sum += mat[i][i]
+        sum += mat[i][len - 1 - i]
+    }
+
+    console.log(mat[Math.floor(len/2)][Math.floor(len/2)])
+    if (len % 2 !== 0) {
+        sum -= mat[Math.floor(len/2)][Math.floor(len/2)]
+    }
+
+    return sum
+
+};
